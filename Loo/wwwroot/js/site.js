c3692d8bc4e96e21a5757026c870c0305688564e
aasmal <aasmal32@bu.edu>
2018-02-14 09:28:15 -0500

    javascript code attempted, needs review - will attempt a jquery instead.

--- Loo/wwwroot/js/site.js
+++ Loo/wwwroot/js/site.js
@@ -1,1 +1,19 @@
 ﻿// Write your JavaScript code.
+function progressbar(sensorID, sensordata) {
+    var sensorType = sensorID[0];
+    var sensordata;
+    if (sensorType == 'W')
+        if (sensordata == 0)
+            return 0;
+        else
+            return 1;
+    if (sensorType == 'R')
+        return (sensordata / 15 * 100);
+    if (sensordata == "S")
+        if (sensordata <= 0)
+            return 0;
+        else if (sensordata >= 90)
+            return 100;
+        else
+            return (sensordata / 90 * 100);
+};
