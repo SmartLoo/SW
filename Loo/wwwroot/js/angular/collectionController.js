4e1da8a26a3d8b0cd00124fd284708ad2d2a3f0b
aasmal <aasmal32@bu.edu>
2018-02-14 13:48:49 -0500

    removed code from the controller, moved it to site.js, added script src for site.js, navbar still okay

--- Loo/wwwroot/js/angular/collectionController.js
+++ Loo/wwwroot/js/angular/collectionController.js
@@ -26,39 +26,7 @@
 
         });
 
-        $scope.progressbar(function () {
-            var sensorType = "W";
-
-            var sensordata = sensordata1;
-            if (sensorType == 'W')
-                if (sensordata == 0) {
-                    window.alert("width: 0%");
-                    return ("width: 0%");
-                }
-                else {
-                    window.alert("width: 100%");
-                    return ("width: 100%");
-                }
-            else if (sensorType == 'R') {
-                window.alert("width: " + (sensordata / 25 * 100) + "%");
-                return ("width: " + (sensordata / 25 * 100) + "%");
-            }
-            else if (sensordata == 'S')
-                if (sensordata <= 0) {
-                    window.alert("width: 0%");
-                    return ("width: 0%");
-                }
-                else if (sensordata >= 90) {
-                    window.alert("width: 100%");
-                    return ("width: 100%");
-                }
-                else {
-                    window.alert("width: " + (sensordata / 90 * 100) + "%");
-                    return ("width: " + (sensordata / 90 * 100) + "%");
-                }
-            setTimeout(progressbar, 5000);
-
-        });
+      
     };
 })();
 
--- Loo/wwwroot/js/site.js
+++ Loo/wwwroot/js/site.js
@@ -1,19 +1,37 @@
 ﻿// Write your JavaScript code.
-function progressbar(sensorID, sensordata) {
-    var sensorType = sensorID[0];
-    var sensordata;
+function progressbar(SensorID, SensorData) {
+    var sensorType = str.slice(sensortype[0]);
+
+    var sensordata = sensordata1;
     if (sensorType == 'W')
-        if (sensordata == 0)
-            return 0;
-        else
-            return 1;
-    if (sensorType == 'R')
-        return (sensordata / 15 * 100);
-    if (sensordata == "S")
-        if (sensordata <= 0)
-            return 0;
-        else if (sensordata >= 90)
-            return 100;
-        else
-            return (sensordata / 90 * 100);
+        if (sensordata == 0) {
+            window.alert("width: 0%");
+            return ("width: 0%");
+        }
+        else {
+            window.alert("width: 100%");
+            return ("width: 100%");
+        }
+    else if (sensorType == 'R') {
+        window.alert("width: " + (sensordata / 25 * 100) + "%");
+        return ("width: " + (sensordata / 25 * 100) + "%");
+    }
+    else if (sensordata == 'S')
+        if (sensordata <= 0) {
+            window.alert("width: 0%");
+            return ("width: 0%");
+        }
+        else if (sensordata >= 90) {
+            window.alert("width: 100%");
+            return ("width: 100%");
+        }
+        else {
+            window.alert("width: " + (sensordata / 90 * 100) + "%");
+            return ("width: " + (sensordata / 90 * 100) + "%");
+        }
+    else
+        window.alert("ERROR");
+    return 2;
+    setTimeout(progressbar, 5000);
+
 };
