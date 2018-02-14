2eaf0ea969eaae12fa0973e15e3741f4298e9ea8
aasmal <aasmal32@bu.edu>
2018-02-14 11:00:07 -0500

    refactored order of script.

--- Loo/wwwroot/js/angular/collectionController.js
+++ Loo/wwwroot/js/angular/collectionController.js
@@ -12,17 +12,54 @@
         vm.Building = "PHO";
         vm.Location = "1FLOORW";
         vm.SensorId = "302";
-       
+
         $(function () {
             $http.get("/api/buildings?clientId=BostonU")
-                .then(function(response) {
+                .then(function (response) {
                     vm.Buildings = response.data;
 
                     $http.get("/api/sensors?clientId=BostonU&buildingName=" + vm.Buildings[0])
-                        .then(function(response) {
+                        .then(function (response) {
                             vm.Sensors = response.data;
-                    });
-                });                                        
+                        });
+                });
+
         });
+
+        $scope.progressbar(function (sensorID, sensordata1) {
+            var sensorType = sensorID[0];
+
+            var sensordata = sensordata1;
+            if (sensorType == 'W')
+                if (sensordata == 0) {
+                    window.alert("width: 0%");
+                    return ("width: 0%");
+                }
+                else {
+                    window.alert("width: 100%");
+                    return ("width: 100%");
+                }
+            if (sensorType == 'R') {
+                window.alert("width: " + (sensordata / 25 * 100) + "%");
+                return ("width: " + (sensordata / 25 * 100) + "%");
+            }
+            if (sensordata == "S")
+                if (sensordata <= 0) {
+                    window.alert("width: 0%");
+                    return ("width: 0%");
+                }
+                else if (sensordata >= 90) {
+                    window.alert("width: 100%");
+                    return ("width: 100%");
+                }
+                else {
+                    window.alert("width: " + (sensordata / 90 * 100) + "%");
+                    return ("width: " + (sensordata / 90 * 100) + "%");
+                }
+            setTimeout(progressbar, 5000);
+
+        });
+
+
     };
 })();
