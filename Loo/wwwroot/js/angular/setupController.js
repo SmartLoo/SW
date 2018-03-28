d3bebe535e2806bab5abaf171a565baae37325f4
Ben Corn <bcorn@bu.edu>
2018-03-28 18:31:32 -0400

    Step 2, Step Animations for sliding in/out of page on completion

---
+++ Loo/wwwroot/js/angular/setupController.js
@@ -0,0 +1,39 @@
+﻿(function () {
+
+    "use strict";
+
+    angular.module("app")
+        .controller("setupController", setupController);
+
+    function setupController($http, $timeout, $scope, $window) {
+
+        var vm = this;
+        vm.CurrentStep = 1;
+
+        vm.AdvanceStep = function() {
+            $('figure').addClass('dissolve');
+            vm.CurrentStep++;
+        }
+
+
+        function removeElement(event) {
+          if (event.animationName === 'slide-out') {
+            event.target.parentNode.removeChild(event.target);
+
+            var stepId = '#step' + vm.CurrentStep.toString();
+            $(stepId).removeClass('hidden-step');
+            $(stepId).addClass('slide-in');
+          }
+
+          if (event.animationName === 'dissolve') {
+              $('#step1').addClass('slide-out');
+          }
+        }
+
+        document.body.addEventListener('animationend',removeElement);
+        document.body.addEventListener('webkitAnimationEnd', removeElement);
+
+    };
+
+
+})();
